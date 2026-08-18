b2d6db04eb4502ea99ad707e723123232c04dafa
Istiak Ahmmed Bishal <167230922+bishal040@users.noreply.github.com>
2026-08-18 23:48:32 +0600

    feat: add more admin emails

--- src/components/pages/AdminLogin.tsx
+++ src/components/pages/AdminLogin.tsx
@@ -7,7 +7,11 @@ import { useAuth } from '../../context/AuthContext';
 const ADMIN_EMAILS = [
   'istiak.ahmmed.bishal@g.bracu.ac.bd',
   'bracudiganta@gmail.com',
-  'mountashiourtasnim@gmail.com'
+  'mountashiourtasnim@gmail.com',
+  'hasna.hena.jui@g.bracu.ac.bd',
+  'chironjeet.joy@bracu.ac.bd',
+  'md.mountashiour.rahman@g.bracu.ac.bd',
+  'tanvir.ahmed.tonmoy@g.bracu.ac.bd'
 ];
 
 export const AdminLogin: React.FC = () => {
